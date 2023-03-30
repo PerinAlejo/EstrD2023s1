c9d385677f1611538b48e41a92266a51fa78cd7f
Alejo Perin <83074266+PerinAlejo@users.noreply.github.com>
2023-03-30 17:57:55 -0300

    Avances ejercicio 2.1

--- tp-3/practica_3.hs
+++ tp-3/practica_3.hs
@@ -69,9 +69,74 @@ pasosHastaTesoro (Cofre os c) = if tieneTesoro os
                                 else 1 + pasosHastaTesoro c
 
 ------------------------------------------------------------------------------------
-hayTesoroEn :: Int -> Camino -> Bool
-hayTesoroEn _     Fin      = False
-hayTesoroEn n   (Nada c)   = pasosHastaTesoro c == n
-hayTesoroEn n (Cofre os c) = pasosHastaTesoro c == n
+-- hayTesoroEn :: Int -> Camino -> Bool
 
-                                
+------------------------------------------------------------------------------------
+alMenosNTesoros :: Int -> Camino -> Bool
+alMenosNTesoros n c = cantidadDeTesoros c >= n
+
+cantidadDeTesoros :: Camino -> Int
+cantidadDeTesoros     Fin      = 0
+cantidadDeTesoros   (Nada c)   = cantidadDeTesoros c
+cantidadDeTesoros (Cofre os c) = unoSi(tieneTesoro os) + cantidadDeTesoros c
+
+-- alMenosNTesoros 2 (Nada (Cofre [Tesoro] (Cofre [Tesoro] Fin)))
+
+--2. Tipos arbóreos
+--2.1. Árboles binarios
+
+data Tree a = EmptyT | NodeT a (Tree a) (Tree a)
+        deriving Show
+
+------------------------------------------------------------------------------------
+sumarT :: Tree Int -> Int
+sumarT       EmptyT        = 0 
+sumarT (NodeT n (a1) (a2)) = n + sumarT a1 + sumarT a2
+
+------------------------------------------------------------------------------------
+sizeT :: Tree a -> Int
+sizeT        EmptyT       = 0
+sizeT (NodeT n (a1) (a2)) = 1 + sizeT a1 + sizeT a2 
+
+------------------------------------------------------------------------------------
+mapDobleT :: Tree Int -> Tree Int
+mapDobleT       EmptyT        = EmptyT
+mapDobleT (NodeT n (a1) (a2)) = (NodeT (n*2) (mapDobleT a1) (mapDobleT a2))
+
+------------------------------------------------------------------------------------
+perteneceT :: Eq a => a -> Tree a -> Bool
+perteneceT x       EmptyT        = False
+perteneceT x (NodeT xa (a1) (a2)) = x == xa || perteneceT x a1 || perteneceT x a2
+
+------------------------------------------------------------------------------------
+aparicionesT :: Eq a => a -> Tree a -> Int
+aparicionesT x       EmptyT         = 0
+aparicionesT x (NodeT xa (a1) (a2)) = unoSi(x==xa) + (aparicionesT x a1) + (aparicionesT x a2)
+
+------------------------------------------------------------------------------------
+leaves :: Tree a -> [a]
+leaves       EmptyT        = []
+leaves (NodeT e (a1) (a2)) = e : leaves a1 ++ leaves a2  
+
+------------------------------------------------------------------------------------
+heightT :: Tree a -> Int
+heightT       EmptyT        = 0
+heightT (NodeT e (a1) (a2)) = 1 + max (heightT a1) (heightT a2)
+
+------------------------------------------------------------------------------------
+mirrorT :: Tree a -> Tree a
+mirrorT       EmptyT        = EmptyT
+mirrorT (NodeT e (a1) (a2)) = (NodeT e (mirrorT a2) (mirrorT a1))
+
+------------------------------------------------------------------------------------
+toList :: Tree a -> [a]
+toList        EmptyT       = []
+toList (NodeT e (a1) (a2)) = toList a1 ++ [e] ++ toList a2
+
+------------------------------------------------------------------------------------
+-- levelN :: Int -> Tree a -> [a]
+-- levelN n        EmptyT       = 
+-- levelN n (NodeT e (a1) (a2)) = 
+
+
+
