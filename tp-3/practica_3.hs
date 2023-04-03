a7b3667c27a6325e56d7d6a28878b4e29bc67b48
Alejo Perin <alejoperin11@gmail.com>
2023-04-03 16:40:59 -0300

    Ejercicio 2.1 Terminado

--- tp-3/practica_3.hs
+++ tp-3/practica_3.hs
@@ -69,7 +69,15 @@ pasosHastaTesoro (Cofre os c) = if tieneTesoro os
                                 else 1 + pasosHastaTesoro c
 
 ------------------------------------------------------------------------------------
--- hayTesoroEn :: Int -> Camino -> Bool
+hayTesoroEn :: Int -> Camino -> Bool
+hayTesoroEn 0      c       = hayTesoroEnLugarActual c
+hayTesoroEn _     Fin      = False
+hayTesoroEn n   (Nada c)   = hayTesoroEn (n-1) c
+hayTesoroEn n (Cofre os c) = hayTesoroEn (n-1) c
+ 
+hayTesoroEnLugarActual :: Camino -> Bool
+hayTesoroEnLugarActual (Cofre os _) = tieneTesoro os
+hayTesoroEnLugarActual      _       = False
 
 ------------------------------------------------------------------------------------
 alMenosNTesoros :: Int -> Camino -> Bool
@@ -80,7 +88,16 @@ cantidadDeTesoros     Fin      = 0
 cantidadDeTesoros   (Nada c)   = cantidadDeTesoros c
 cantidadDeTesoros (Cofre os c) = unoSi(tieneTesoro os) + cantidadDeTesoros c
 
--- alMenosNTesoros 2 (Nada (Cofre [Tesoro] (Cofre [Tesoro] Fin)))
+------------------------------------------------------------------------------------
+cantTesorosEntre :: Int -> Int -> Camino -> Int
+cantTesorosEntre n1 n2     Fin      = 0
+cantTesorosEntre n1 n2   (Nada c)   = cantTesorosEntre n1 n2 c
+cantTesorosEntre n1 n2 (Cofre os c) = cantTesorosEntre n1 n2 c
+
+
+
+
+
 
 --2. Tipos arbóreos
 --2.1. Árboles binarios
@@ -134,9 +151,40 @@ toList        EmptyT       = []
 toList (NodeT e (a1) (a2)) = toList a1 ++ [e] ++ toList a2
 
 ------------------------------------------------------------------------------------
--- levelN :: Int -> Tree a -> [a]
--- levelN n        EmptyT       = 
--- levelN n (NodeT e (a1) (a2)) = 
+levelN :: Int -> Tree a -> [a]
+levelN _        EmptyT       = []
+levelN 0 (NodeT e (a1) (a2)) = e : []
+levelN n (NodeT e (a1) (a2)) = levelN (n-1) a1 ++ levelN (n-1) a2
+
+------------------------------------------------------------------------------------
+listPerLevel :: Tree a -> [[a]]
+listPerLevel       EmptyT       = []
+listPerLevel (NodeT e (a1) (a2)) = [e] : juntarNiveles (listPerLevel a1) (listPerLevel a2)
+
+juntarNiveles :: [[a]] -> [[a]] -> [[a]]
+juntarNiveles    []       yss   = yss
+juntarNiveles    xss      []    = xss
+juntarNiveles (xs:xss) (ys:yss) = (xs ++ ys) : juntarNiveles xss yss
+
+------------------------------------------------------------------------------------
+ramaMasLarga :: Tree a -> [a]
+ramaMasLarga       EmptyT       = []
+ramaMasLarga (NodeT e (a1) (a2)) = e : laMasLarga (ramaMasLarga a1) (ramaMasLarga a2)
+
+laMasLarga :: [a] -> [a] -> [a]
+laMasLarga xs ys = if (length ys) > (length xs)             
+                      then ys
+                      else xs
+
+------------------------------------------------------------------------------------
+todosLosCaminos :: Tree a -> [[a]]
+todosLosCaminos       EmptyT        = []
+todosLosCaminos (NodeT e (a1) (a2)) = [e] : consACada e (todosLosCaminos a1) ++ consACada e (todosLosCaminos a2)
+
+consACada :: a -> [[a]] -> [[a]]
+consACada x    []    = []
+consACada x (xs:xss) = (x:xs) : consACada x xss
 
 
+t1 = (NodeT 1 (NodeT 2(NodeT 3(EmptyT)(NodeT 4 (EmptyT)(EmptyT)))(EmptyT)) (NodeT 3(NodeT 4(EmptyT)(EmptyT)) EmptyT))
 
